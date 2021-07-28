a5a38eeb00808530f5841266ad5a3713a046f888
emwitchcraft <60982762+emwitchcraft@users.noreply.github.com>
2021-07-28 18:44:56 -0400

    Add files via upload

---
+++ routeShorts.py
@@ -0,0 +1,35 @@
+from dis import dis
+import bigDumbBird
+import sys
+
+dumbBirdFile = sys.argv[1] if len(sys.argv) > 1 else input('gimme file:')
+
+board = bigDumbBird.Board(dumbBirdFile, disableBackup=False)
+scr = bigDumbBird.ScriptWriter(dumbBirdFile)
+
+signals = board.getSignals()
+
+def distance(x0, y0, xf, yf):
+    delX = xf - x0
+    delY = y0 - yf
+    return ((delX ** 2) + (delY ** 2)) ** 0.5
+
+def getCoords(wire):
+    f = lambda w,i: float(w.get(i))
+    return f(wire, 'x1'), f(wire, 'y1'), f(wire, 'x2'), f(wire, 'y2')
+
+def getMid (x0, y0, xf, yf):
+    return x0 + ((xf - x0) * 0.5), y0 + ((yf - y0) * 0.5)
+
+onlyAirWires = lambda wire: wire.get('layer') == '19'
+skipSignalsWithPlanePour = lambda signal: signal.find('polygon') is None
+
+airWires = filter(onlyAirWires, [wire for signal in filter(skipSignalsWithPlanePour, signals) for wire in signal.findall('wire')])
+
+maxDistance = 3.5
+for airWire in airWires:
+    x0, y0, xf, yf = getCoords(airWire)
+    if distance(x0, y0, xf, yf) < maxDistance:
+        mid = getMid(x0, y0, xf, yf)
+        scr += f'route ({x0} {y0}) ({xf} {yf})'
+scr.save()
